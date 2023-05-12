6ad6b9c394f49f88f431c4d9419e0489a851a087
Max Church <maxchurch01@gmail.com>
2023-05-12 15:22:31 +0100

    test

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from fastapi import FastAPI, UploadFile, File
 import io
 import wave
-from pydantic import BaseModel
-
 
 app = FastAPI()
 
 
 class ButtonData(BaseModel):
-    button_pressed: bool
+    audio_data: str
 
 
 @app.post("/")
 async def handle_button_data(data: ButtonData):
-    if data.button_pressed:
-        # You can customize the response based on your application logic
-        led = "green"
-        return led
-    return "No action"
+    audio_data = data.audio_data
+    audio_file = io.BytesIO(audio_data)
+
+    # return how long the audio file is in seconds
+
+    try:
+        with wave.open(audio_file, 'rb') as audio:
+            return audio.getnframes() / audio.getframerate()
+    except Exception as e:
+
+        return "Ok it didnt work in the fastapi: " + str(e)
