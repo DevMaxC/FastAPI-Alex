18f0b129a617235409380391650d31a1e85a037a
Max Church <maxchurch01@gmail.com>
2023-05-12 15:44:06 +0100

    go

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ app = FastAPI()
 
 class ButtonData(BaseModel):
     audio_data: str
+    is_end: bool
 
 
 @app.post("/")
@@ -17,7 +18,26 @@ async def handle_button_data(data: ButtonData):
     # if there isn't, create the file and write the data to it
     with open("audio.txt", "a") as f:
         f.write(data.audio_data)
-    return {"message": "Data received"}
+
+        if data.is_end:
+            # convert the audio.txt file to a wav file
+            with open("audio.txt", "r") as f:
+                audio_data = f.read()
+                audio_data = audio_data.split(",")
+                audio_data = [int(i) for i in audio_data]
+                audio_data = bytes(audio_data)
+                with wave.open("audio.wav", "wb") as wav_file:
+                    wav_file.setnchannels(1)
+                    wav_file.setsampwidth(2)
+                    wav_file.setframerate(16000)
+                    wav_file.writeframes(audio_data)
+                    duration = wav_file.getnframes() / float(wav_file.getframerate())
+            # send the wav file to the speech to text api
+            # return the audio length in seconds: Datalength: n seconds
+
+            return {"message": "Datalength: " + str(duration) + " seconds"}
+        else:
+            return {"message": "Data received"}
 
 # delete the audio.txt file
 
