fda614f917d1eb0dada08cd48c6f64cbb2f99daf
Max Church <maxchurch01@gmail.com>
2023-05-12 14:55:24 +0100

    yoyoyoy

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ import wave
 app = FastAPI()
 
 
-@app.post("/upload")
-async def receive_audio(audio: UploadFile = File(...)):
-    contents = await audio.read()
-    audio_data = io.BytesIO(contents)
+class ButtonData(BaseModel):
+    button_pressed: bool
 
-    with wave.open(audio_data, "rb") as wav_file:
-        n_frames = wav_file.getnframes()
-        frame_rate = wav_file.getframerate()
-        duration = n_frames / float(frame_rate)
 
-    return {"length": duration}
+@app.post("/")
+async def handle_button_data(data: ButtonData):
+    if data.button_pressed:
+        # You can customize the response based on your application logic
+        led = "green"
+        return led
+    return "No action"
