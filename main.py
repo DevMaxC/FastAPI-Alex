8bcf11e740cef4f61d18c98659b807a56f663509
Max Church <maxchurch01@gmail.com>
2023-05-12 16:32:56 +0100

    openai

--- main.py
+++ main.py
@@ -3,13 +3,54 @@ import io
 import wave
 from pydantic import BaseModel
 import base64
+import os
+import openai
+import requests
+import requests
 
+CHUNK_SIZE = 1024
+url = "https://api.elevenlabs.io/v1/text-to-speech/q9tuMLqYtfvnRwpnCxhr"
+
+xKey = os.getenv("XI_API_KEY")
+headers = {
+    "Accept": "audio/mpeg",
+    "Content-Type": "application/json",
+    "xi-api-key": xKey,
+}
+
+data = {
+    "text": "Hi! My name is Bella, nice to meet you!",
+    "model_id": "eleven_monolingual_v1",
+    "voice_settings": {
+        "stability": 0,
+        "similarity_boost": 0
+    }
+}
+
+response = requests.post(url, json=data, headers=headers)
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+print(os.getenv("OPENAI_API_KEY"))
 app = FastAPI()
 
 
 class ButtonData(BaseModel):
     audio_data: str
     is_end: bool
+
+
+@app.get("/")
+async def root():
+    # if there is no file in the directory called "output.mp3", respond with a message saying that there is no file
+    # if there is a file in the directory called "output.mp3", respond with the file in base64 format
+
+    if os.path.isfile("output.mp3"):
+        with open("output.mp3", "rb") as f:
+            encoded_audio = base64.b64encode(f.read())
+            return {encoded_audio}
+    else:
+        return {"No file found"}
 
 
 @app.post("/")
@@ -32,12 +73,18 @@ async def handle_button_data(data: ButtonData):
                 f.setframerate(16000)
                 f.writeframes(decoded_audio_data)
 
+            audio_file = open("audio.wav", "rb")
+            print("sending to openai...")
+            transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+            print(transcript)
+
             filesize = f.getnframes() * f.getsampwidth()
             duration = filesize / 16000 / 2
 
             with open("audio.txt", "w") as f:
                 f.write("")
 
-            return {"message": "Datalength: " + str(duration) + " seconds"}
+            return {"message": transcript}
         else:
             return {"message": "Data received"}
